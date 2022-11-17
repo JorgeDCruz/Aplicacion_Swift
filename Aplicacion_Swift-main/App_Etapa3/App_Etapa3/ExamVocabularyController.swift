91914f09760548a30179231a8f14b156670a1cf3
Max Jauregui <a01570673@tec.mx>
2022-11-16 18:41:21 -0600

    Examen de Vocabulario
    Lee de la base de datos para posicionar las opciones, la pregunta y la imagen de la seña

---
+++ Aplicacion_Swift-main/App_Etapa3/App_Etapa3/ExamVocabularyController.swift
@@ -0,0 +1,143 @@
+//
+//  ExamVocabularyController.swift
+//  App_Etapa3
+//
+//  Created by Alumno on 15/11/22.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseCore
+
+class ExamVocabularyController: UIViewController {
+    
+    let db = Firestore.firestore()
+    let collection = "exams/exam1/grammar"
+    var questionArray = [17,1,3,4,5,6,7,8,9,10]
+    
+    // Variables utilizadas
+    var currentQuestion = 0
+    var rightAnswerPlacement:UInt32 = 0
+    var points = 0
+    @IBOutlet weak var userProgress: UIProgressView!
+    @IBOutlet weak var questionCounter: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getQuestions()
+        newQuestion()
+    }
+    
+    @IBAction func checkAnswer(_ sender: AnyObject) {
+        
+        self.userProgress.progress += 0.1
+        
+        if (sender.tag == Int(rightAnswerPlacement)) {
+            print("RIGHT!")
+            points += 1
+            print(points)
+        }
+        else {
+            print("WRONG!")
+        }
+        
+        if (currentQuestion != 10) {
+            newQuestion()
+        }
+    }
+    
+    // Se obtienen las 10 preguntas de manera aleatoria del banco de preguntas
+    func getQuestions(){
+        db.collection(collection).getDocuments() {
+            (querySnapshot, err) in
+            
+            if let err = err {
+                print("Error getting documents: \(err)")
+            }
+            else {
+                let countQuery = querySnapshot?.count
+                for i in 0...9 {
+                    let num = Int.random(in:  1..<(countQuery ?? 10))
+                    self.questionArray.insert(num, at: i)
+                }
+            }
+        }
+    }
+    
+    func newQuestion() {
+        // Se desplega el numero de pregunta actual
+        self.questionCounter.text = String(currentQuestion + 1)
+        // Posicion de la opcion correcta
+        rightAnswerPlacement = arc4random_uniform(4) + 1
+        
+        // Creacion de boton
+        var button:UIButton = UIButton()
+        
+        var x = 1
+        
+        // Se lee el documento
+        let documentName = "question" + String(self.questionArray[currentQuestion])
+        let docRef = db.collection(collection).document(documentName)
+        
+        // Se lee el documento
+        docRef.getDocument { (document, error) in
+            guard let document = document, document.exists else {
+                return
+            }
+            
+            let dataDescription = document.data()
+            
+            // URL de la imagen
+            let url = dataDescription?["image"] as? String ?? ""
+            
+            let imageUrl:URL = URL(string: url)!
+
+            DispatchQueue.global().async {
+                let data = try? Data(contentsOf: imageUrl)
+                DispatchQueue.main.async {
+                    self.image.image = UIImage(data: data!)
+                }
+            }
+            
+            // self.downloadImage(from: imageUrl)
+            // Se muestra la pregunta en pantalla
+            self.questionLabel.text = dataDescription?["question"] as? String ?? ""
+            
+            // Se cambia el titulo del boton para representar las opciones
+            for i in 1...4 {
+                button = self.view.viewWithTag(i) as! UIButton
+                
+                if (i == Int(self.rightAnswerPlacement)) {
+                    let name = dataDescription?["correctans"] as? String ?? ""
+                    button.setTitle(name, for: .normal)
+                }
+                // Se utilizan las opciones "choice1", "choice2" y "choice3" de la BD
+                else {
+                    let choice = "choice" + String(x)
+                    let name = dataDescription?[choice] as? String ?? ""
+                    button.setTitle(name, for: .normal)
+                    x += 1
+                }
+            }
+            self.currentQuestion += 1
+        }
+    }
+    
+    func downloadImage(from url: URL) {
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            // always update the UI from the main thread
+            DispatchQueue.main.async() { [weak self] in
+                self?.image.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+}
---
+++ Aplicacion_Swift-main/README.md
@@ -0,0 +1,26 @@
+# Aplicación iOS de Lenguaje de Señas
+
+## Descripción del proyecto
+El proyecto consiste en el desarrollo de un prototipo de una aplicación para iOS para el aprendizaje de lenguaje de señas mexicano a través de lecciones y exámenes. La aplicación se esta desarrollando a la mano con la compañía "John Deere". 
+
+La aplicación se esta desarrollando en el lenguaje **"Swift"** para dispositivos iOS, a la mando de una base de datos en **Firebase**. El desarrollo se esta realizando en el IDE **"XCode 13"**, exclusivo para dispositivos con MacOS.
+
+Al final del desarrollo del proyecto se entregará un prototipo completo, el cual contará con todos los aditamentos adicionales para el uso y mantenimiento de la aplicación, como bases de datos, medios de comunicación, protocolos de seguridad, etc.
+
+También se contará con una documentación extensa y detallada, tanto para el uso de la aplicación desde el punto de vista de varios usuarios en la forma de un manual de usuario, como la descripción del funcionamiento interno de la aplicación y sus componentes
+
+
+<br></br>
+![alt text](image_2022-11-15_091517543.png)
+## Instrucciones 
+## Autores: 
+### - Equipo: Relámpagos Marquinhos
+-  Mauricio Cuadros A01236054
+-  Jorge Daniel Cruz A01634536
+-  Carlos Alejandro Morales A00830435
+-  Rodolfo Maximiliano Jáuregui A01570673
+### ¿Cómo visuaizar y editar el proyecto?
+Es necesario tener una mac con Xcode instalado y abrir este proyecto. 
+
+### Creado por Equipo Relámpagos Marquinhos:
+#### Mauricio Cuadros, Jorge Cruz, Alejandro Morales y Maximiliano Jáuregui
